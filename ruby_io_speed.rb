2c855833bdd25f479cd95b9a60620999d47bb50b
Hrvoje Šimić <shime@twobucks.co>
2015-10-20 23:31:15 +0200

    update ruby benchmark to show miliseconds

--- ruby_io_speed.rb
+++ ruby_io_speed.rb
@@ -20,16 +20,15 @@ include ImageProcessing::MiniMagick
 require "open-uri"
 require "benchmark"
 
-duration = Benchmark.realtime do
-  file = open("http://www.planwallpaper.com/static/images/2022725-wallpaper_625864_Iz6NK8G.jpg")
+start = Time.now.to_f
+file = open("http://www.planwallpaper.com/static/images/2022725-wallpaper_625864_Iz6NK8G.jpg")
 
-  version_800 = resize_to_limit!(file, 800, 800)
-  version_500 = resize_to_limit(version_800, 500, 500)
-  version_300 = resize_to_limit(version_500, 300, 300)
+version_800 = resize_to_limit!(file, 800, 800)
+version_500 = resize_to_limit(version_800, 500, 500)
+version_300 = resize_to_limit(version_500, 300, 300)
 
-  bucket.object("large.jpg").put(body: version_800)
-  bucket.object("medium.jpg").put(body: version_500)
-  bucket.object("small.jpg").put(body: version_300)
-end
+bucket.object("large.jpg").put(body: version_800)
+bucket.object("medium.jpg").put(body: version_500)
+bucket.object("small.jpg").put(body: version_300)
 
-puts "Took: #{duration}s"
+puts "Took: #{((Time.now - start).to_f * 1000).to_i}ms"
