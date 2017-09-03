94dcfc154a170a856371d5d6267cb2f5c391fdc1
Janko Marohnić <janko.marohnic@gmail.com>
2017-09-03 14:57:32 +0200

    Update Ruby benchmark

--- ruby_io_speed.rb
+++ ruby_io_speed.rb
@@ -1,22 +1,20 @@
 require "vips"
+
+ORIGINAL = "cakes.jpg"
+
 SIZES = {
   large:  [800, 800],
   medium: [500, 500],
   small:  [300, 300],
 }
 
-image = Vips::Image.new_from_file("bridge.jpg")
-start = Time.now.to_f
-threads = []
+start = Time.now
 
+SIZES.map do |size, (width, height)|
+  Thread.new do
+    name = size.to_s + ".jpg"
+    Vips::Image.thumbnail(ORIGINAL, width).write_to_file(name)
+  end
+end.each(&:join)
 
-SIZES.each do |size, (width, height)|
-    threads << Thread.new {
-      factor = image.width.to_f / width
-      filename = size.to_s + ".jpg"
-      image.shrink(factor, factor).write_to_file(filename)
-    }
-end
-
-threads.each(&:join)
-puts "Took: #{((Time.now - start).to_f * 1000).to_i}ms"
+puts "Took: #{((Time.now - start) * 1000).to_i}ms"
